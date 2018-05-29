53c2e9def50158a7d1897e5df28f3ade62f963e5
wanghui1996 <1242552744@qq.com>
2018-05-29 21:14:31 +0800

    ceshi

--- Todo/main.py
+++ Todo/main.py
@@ -1,11 +1,21 @@
-from flask import  Flask
+from flask import  Flask, render_template
 
 
 
 app  = Flask(__name__)
+
+
 @app.route('/')
-def hello():
-    return  'hello work'
+def index():
+    todo = [
+
+        "ceshi",
+        "ceshi",
+        "ceshi"
+
+    ]
+
+    return  render_template('index.html',data = todo)
 
 
 
