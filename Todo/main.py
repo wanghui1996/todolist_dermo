321221832dc230f8a88b0eaf0fbe46d6f50e31c9
wanghui1996 <1242552744@qq.com>
2018-05-29 20:42:01 +0800

    wanghui

---
+++ Todo/main.py
@@ -0,0 +1,17 @@
+from flask import  Flask
+
+
+
+app  = Flask(__name__)
+@app.route('/')
+def hello():
+    return  'hello work'
+
+
+
+
+
+
+if __name__ == '__main__':
+    # aap = Flask()
+    app.run(debug = True)
