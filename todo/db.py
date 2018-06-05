25f00f54c21f620ff0f9ae45c31f78bafb80f998
wanghui1996 <1242552744@qq.com>
2018-06-05 19:29:21 +0800

    wanzhang

--- todo/db.py
+++ todo/db.py
@@ -2,22 +2,22 @@ import sqlite3
 
 
 class TodoDB():
-    def  __init__(self):
+    def __init__(self):
         self.conn = sqlite3.connect('test.db')
+
     def cursor(self):
         return self.conn.cursor()
+
     def close(self):
         self.conn.close()
+
     def commit(self):
         self.conn.commit()
-
-
-
 
     def read_all(self):
         conn = sqlite3.connect('test.db')
         cursor = conn.cursor()
-        cursor.execute('select id, content from todo order by id desc')
+        cursor.execute('select id, content, status from todo order by id desc')
         data = cursor.fetchall()
         cursor.close()
         conn.close()
@@ -26,43 +26,66 @@ class TodoDB():
 
     def read(self, todo_id):
         cursor = self.cursor()
-        cursor = cursor.execute('select id, content from todo where id=?', (todo_id,))
+        cursor = cursor.execute('select id, content, status from todo where id=?', (todo_id,))
         data = cursor.fetchone()
         cursor.close()
-        return  data
-
+        return data
 
     def init_db(self):
-
         conn = sqlite3.connect('test.db')
         cursor = conn.cursor()
         # cursor.execute ("drop table if exists todo")
-        # cursor.execute("create table todo (id integer primary key AUTOINCREMENT, content varchar(50))")
+        cursor.execute("create table if not exists todo (id integer primary key AUTOINCREMENT, content varchar(50))")
         cursor.close()
         conn.commit()
         conn.close()
 
-    def test(self):
-        pass
+    def migrate_latest(self):
+        # 数据库迁移
+        self.init_db()
+        self.s2_add_status_column()
+
+    # def test(self):
+    #     pass
 
     def delete(self, todo_id):
         cursor = self.cursor()
-        cursor = cursor.execute('delete from todo where id=?', (todo_id, ))
+        cursor = cursor.execute('delete from todo where id=?', (todo_id,))
         cursor.close()
         self.commit()
         return
-        print('delete', todo_id)
+        # print('delete', todo_id)
 
     def create(self, text):
         cursor = self.cursor()
-        cursor = cursor.execute('insert into todo(content) values (?)', (text, ))
+        cursor = cursor.execute('insert into todo(content) values (?)', (text,))
         cursor.close()
         self.commit()
 
 
+    def s2_add_status_column(self):
+        conn = sqlite3.connect('test.db')
+        cursor = self.cursor()
+        # cursor.execute("drop table if exists todo")
+        cursor.execute("alter table todo add column status varchar default 'done'")
+        cursor.close()
+        conn.commit()
+        conn.close()
 
-if __name__ =='__main__':
+
+    def update_status(self, todo_id, status):
+        cursor = self.cursor()
+        cursor = cursor.execute('update todo set status = ? where id =?', (status, todo_id))
+        self.commit()
+        data = cursor.fetchone()
+        print(data)
+        cursor.close()
+        return data
+
+
+if __name__ == "__main__":
     db = TodoDB()
-    db.init_db()
-    a = db.read_all()
-    print(a)
+    # db.init_db()
+    # a = db.read_all()
+    db.migrate_latest()
+    # print(a)
--- todo/main.py
+++ todo/main.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, jsonify, request
-from todo.db import  TodoDB
+from todo.db import TodoDB
 
-
-app  = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -10,17 +9,18 @@ def index():
     db = TodoDB()
     todo = db.read_all()
 
-    return  render_template('index.html', data = todo)
+    return render_template('index.html', data=todo)
 
 
-@app.route('/todo/<int:todo_id>',methods = ["DELETE"])
+@app.route('/todo/<int:todo_id>', methods=["DELETE"])
 def delete(todo_id):
     db = TodoDB()
     db.delete(todo_id)
     result = db.read(todo_id)
     db.close()
-    return  jsonify({'existed':True}) if result else \
+    return jsonify({'existed': True}) if result else \
         jsonify({'existed': False})
+
 
 @app.route('/todo', methods=['POST'])
 def add():
@@ -29,6 +29,21 @@ def add():
     db.create(data['text'])
     db.close()
     return 'ok'
+
+
+@app.route('/todo/<int:todo_id>', methods=['PUT'])
+def flip_status(todo_id):
+    db = TodoDB()
+    todo = db.read(todo_id)
+    db.close()
+    if todo:
+        status = todo[2]
+        status = 'done' if status == 'doing' else 'doing'
+        db.update_status(todo_id, status)
+        db.commit()
+    db.close()
+    return 'ok'
+
 
 if __name__ == '__main__':
     # aap = Flask()
